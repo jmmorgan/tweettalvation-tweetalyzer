7efaf7d81b4fdb9c6a207768c96e6fbedd4f0070
Your Name <joseph_m_morgan@yahoo.com>
2016-12-12 15:08:26 -0500

    First pass at tweet layout;

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-
+    @trump_tweets = Tweet.most_recent_from_author(25073877)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,6 @@
 class Tweet < ApplicationRecord
+
+  def self.most_recent_from_author(twitter_user_id, limit = 10)
+    Tweet.where(twitter_user_id: twitter_user_id).order('twitter_id DESC').limit(10)
+  end
 end
