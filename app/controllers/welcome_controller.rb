bf6ebd1949ea8499d15beb3cf2c675cbc575563c
Your Name <joseph_m_morgan@yahoo.com>
2016-12-13 01:27:07 -0500

    Dynamically populate names and avatars on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
 
   def index
-    @trump_tweets = Tweet.most_recent_from_author(25073877)
+    trump_id = 25073877
+    @trump_tweets = Tweet.most_recent_from_author(trump_id)
+    @trump = TwitterUser.find_by(twitter_user_id: trump_id)
     @review_stats = Tweet.review_stats(@trump_tweets)
   end
 end
