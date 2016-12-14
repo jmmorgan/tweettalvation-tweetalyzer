0ece5d44eb4e43e33232db908ff00fe1331206e4
Your Name <joseph_m_morgan@yahoo.com>
2016-12-13 22:31:39 -0500

    Added basic review pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,43 @@ class WelcomeController < ApplicationController
 
   def index
     trump_id = 25073877
-    @trump_tweets = Tweet.most_recent_from_author(trump_id)
+    @trump_tweets = Tweet.most_recent_from_author(trump_id, 25)
     @trump = TwitterUser.find_by(twitter_user_id: trump_id)
     @review_stats = Tweet.review_stats(@trump_tweets)
   end
 
   def expand_reviews
-    twitter_id = params[:twitter_id]
-    sentiment = params[:sentiment]
-    @replies = Tweet.replies(twitter_id, sentiment)
+    @twitter_id = params[:twitter_id]
+    @sentiment = params[:sentiment]
+    @total_count = params[:count].to_i
+    @page = (params[:page] || 1).to_i
+    @limit = 25 # hard code for now
+    @final_page = (@total_count - 1)/@limit + 1
+    _build_pagination_array
+    offset = (@page -1 ) * @limit
+    @replies = Tweet.replies(@twitter_id, @sentiment, offset, @limit)
+  end
+
+  def _build_pagination_array
+    @pagination_array = []
+    # get current page offset
+    page_offset = ((@page - 1)/  5 ) * 5
+    if (page_offset > 0)
+      @pagination_array << [1, 1]
+      @pagination_array << ['..', page_offset]
+    end
+    ((page_offset + 1)..(page_offset + 5)).each do |page|
+      if (page <= @final_page)
+        @pagination_array << [page, page]
+      end
+    end
+    if (page_offset + 6 < @final_page)
+      @pagination_array << ['..', page_offset + 6]
+    end
+    if (page_offset + 5 < @final_page)
+      @pagination_array << [@final_page, @final_page]
+    end
+
+    Rails.logger.debug @pagination_array.inspect
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,7 @@
 class Tweet < ApplicationRecord
 
   def self.most_recent_from_author(twitter_user_id, limit = 10)
-    Tweet.where(twitter_user_id: twitter_user_id).order('twitter_id DESC').limit(10)
+    Tweet.where(twitter_user_id: twitter_user_id).order('twitter_id DESC').limit(limit)
   end
 
   def self.review_stats(tweets)
