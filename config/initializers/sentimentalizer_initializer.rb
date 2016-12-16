af17f85a3f2480290aa4216d9b5f94dd868a7458
Your Name <joseph_m_morgan@yahoo.com>
2016-12-15 22:52:06 -0500

    Introduce additional training data

--- config/initializers/sentimentalizer_initializer.rb
+++ config/initializers/sentimentalizer_initializer.rb
@@ -1,7 +1,19 @@
 require 'sentimentalizer'
 
+class Sentimentalizer
+
+  def self.additional_training
+    @analyser.train_positive "#{File.dirname(__FILE__)}/../../lib/additional_sentiment_training_data/positive"
+    @analyser.train_negative"#{File.dirname(__FILE__)}/../../lib/additional_sentiment_training_data/negative"
+  end
+end
+
 Tweetalyzer::Application.configure do
   config.after_initialize do
     Sentimentalizer.setup
+    # Do additional training
+    puts "Performing additional sentiment training..."
+    Sentimentalizer.additional_training
+    puts "Additional sentiment training complete."
   end
 end
