b2bc03ace0c556f26e46bc416bea3615ddc21e85
Your Name <joseph_m_morgan@yahoo.com>
2016-12-22 02:41:52 -0500

    First cut at completed timeline graph

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,7 +1,8 @@
 module WelcomeHelper
   def _replies_timeline_data_csv(replies_timeline_data)
-    # Get times and sort
-    date_times = replies_timeline_data.to_a.map{|d| DateTime.parse(d[:time]).to_i}.uniq.sort
+    # Get date times for x axis
+    date_times = _build_date_times_axis(replies_timeline_data)
+
     counts_map = {
       -1 => Hash.new(0),
       0 => Hash.new(0),
@@ -11,27 +12,38 @@ module WelcomeHelper
     replies_timeline_data.to_a.each do |d|
       counts_map[d[:sentiment]][DateTime.parse(d[:time]).to_i] = d[:count]
     end
-    Rails.logger.info counts_map
-    result = _build_time_values_row(date_times)
+    
+    result = "Time,Positive,Neutral,Negative\n"
 
-    result += "\nNegative," + date_times.map{|d| counts_map[-1][d] }.join(',')
-    result += "\nNeutral," + date_times.map{|d| counts_map[0][d] }.join(',')
-    result += "\nPositive," + date_times.map{|d| counts_map[1][d]  }.join(',')
+    date_times.each do |d|
+      dt = Time.at(d).to_datetime
+      if (dt.hour == 0)
+        result += "#{dt.strftime('%b %d')},"
+      else
+        result += "#{dt.strftime('%H:00')},"
+      end
+
+      result += "#{counts_map[1][d]},#{counts_map[0][d]},#{counts_map[-1][d]}\n"
+    end
 
     result
   end
 
-  def _build_time_values_row(date_times)
-    result = 'Time'
-    date_times.each do |d|
-      d = Time.at(d).to_datetime
-      if (d.hour == 0)
-        result += "," + d.strftime('%b %d')
-      else
-        result += "," + d.strftime('%H:00')
+  def _build_date_times_axis(replies_timeline_data)
+    sorted_times = replies_timeline_data.to_a.map{|d| DateTime.parse(d[:time]).to_i}.uniq.sort
+
+    max = sorted_times.last
+    min = sorted_times.first
+
+    # Append missing hours and resort
+    next_hour = (min + 3600) - min % 3600
+    while(next_hour < max)
+      if(!sorted_times.include?(next_hour))
+        sorted_times << next_hour
       end
+      next_hour += 3600
     end
 
-    result
+    sorted_times.sort
   end
 end
