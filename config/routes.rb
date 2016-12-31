d19f334f96100c4df0441a4bf9d6e4f1f917057f
Your Name <joseph_m_morgan@yahoo.com>
2016-12-30 21:07:47 -0500

    Add basic navigation

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index', via: ['get']
+  match '/welcome', to: 'welcome#index', via: ['get']
 
   match '/expand_reviews', to: 'welcome#expand_reviews', via: ['get', 'post']
   match '/expand_timeline_graph', to: 'welcome#expand_timeline_graph', via: ['get', 'post']
