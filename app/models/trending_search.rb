6ee614682c4f5034e1a2b8f5fdd09a194bba8eba
Your Name <joseph_m_morgan@yahoo.com>
2017-01-01 15:24:02 -0500

    Ensure unique search terms

--- app/models/trending_search.rb
+++ app/models/trending_search.rb
@@ -4,5 +4,6 @@ class TrendingSearch < ApplicationRecord
   # production environment
   def self.recent(limit=10, since=1.hour.ago)
     TrendingSearch.order('created_at DESC').limit(10).to_a.select{|ts| ts.created_at.to_i >= since.to_i}
+      .uniq{|ts| ts.terms}
   end
 end
