bbc083127f3ee8a72eeb3c9bb79a5cb6102b026c
Your Name <joseph_m_morgan@yahoo.com>
2017-01-06 12:19:46 -0500

    Account for corner cases where troll_candidates.statuses_count = 0

--- app/models/troll_candidate.rb
+++ app/models/troll_candidate.rb
@@ -8,7 +8,8 @@ class TrollCandidate < ApplicationRecord
                   FROM troll_candidates tc
                   INNER JOIN twitter_users tu ON (tc.twitter_user_id = tu.twitter_user_id)
                   INNER JOIN tweets t ON (tc.twitter_user_id = t.twitter_user_id AND t.sentiment = 1)
-                  WHERE tc.synched_with_twitter_at IS NOT NULL and tc.trump_related_statuses_count > 0
+                  WHERE tc.synched_with_twitter_at IS NOT NULL AND tc.trump_related_statuses_count > 0
+                  AND tc.statuses_count > 0
                   ORDER BY (tc.trump_related_statuses_count * 1.0/tc.statuses_count) DESC) as x
               
               LIMIT ?"
