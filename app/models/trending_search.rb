a3a008164d573e67633ec35512e87180b6c0970a
Your Name <joseph_m_morgan@yahoo.com>
2017-01-11 00:42:25 -0500

    Fall back to most recent trending searches when Trends fetchComponent service is unavailable."

--- app/models/trending_search.rb
+++ app/models/trending_search.rb
@@ -2,8 +2,15 @@ class TrendingSearch < ApplicationRecord
 
   # All this is for Q & D demos.  WIll document and test before I use any of this in a real
   # production environment
-  def self.recent(limit=10, since=1.hour.ago)
-    TrendingSearch.order('created_at DESC').limit(10).to_a.select{|ts| ts.created_at.to_i >= since.to_i}
+  def self.recent(limit=10, since=1.hour.ago, fallback_to_older=true)
+    searches = result = TrendingSearch.order('created_at DESC').limit(10).to_a
+    result = searches.select{|ts| ts.created_at.to_i >= since.to_i}
       .uniq{|ts| ts.terms}
+    # Quick and dirty fix. Fallback
+    if(result.empty? && fallback_to_older)
+      result = searches.uniq{|ts| ts.terms}
+    end
+
+    result
   end
 end
